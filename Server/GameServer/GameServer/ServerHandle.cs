860add4c6424451d7bdfdcd7b06d1365e8f9f355
Joseph Chen <happypoetjo@gmail.com>
2020-07-30 17:02:59 +0800

    initial commit

---
+++ Server/GameServer/GameServer/ServerHandle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer
+{
+    class ServerHandle
+    {
+        public static void WelcomeReceived(int _fromClient, Packet _packet) {
+            int _ClientIdCheck = _packet.ReadInt();
+            string _username = _packet.ReadString();
+
+            Console.WriteLine($"{Server.clients[_fromClient].tcp.socket.Client.RemoteEndPoint} " +
+                $"connected successfully and is now player {_fromClient}");
+            if (_fromClient != _ClientIdCheck) {
+                Console.WriteLine($"Player \"{_username}\"(ID: {_fromClient}) has assumed the wrong ID ({_ClientIdCheck})!");
+            }
+        }
+    }
+}
